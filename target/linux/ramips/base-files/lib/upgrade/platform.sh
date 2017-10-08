2edd5a9fc14a6ae99b727dfe4cb11d6cc3255c79
Chen Minqiang <ptpt52@gmail.com>
2017-10-08 22:07:58 +0800

    Netgear R6220: fix flash layout
    it look there are three different flash layouts
    add model r6220b r6220a to met layout of his device

--- target/linux/ramips/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/base-files/lib/upgrade/platform.sh
@@ -295,6 +295,8 @@ platform_check_image() {
 		;;
 	hc5962|\
 	mir3g|\
+	r6220b|\
+	r6220a|\
 	r6220|\
 	ubnt-erx|\
 	ubnt-erx-sfp)
@@ -352,6 +354,8 @@ platform_do_upgrade() {
 	case "$board" in
 	hc5962|\
 	mir3g|\
+	r6220b|\
+	r6220a|\
 	r6220|\
 	ubnt-erx|\
 	ubnt-erx-sfp)
