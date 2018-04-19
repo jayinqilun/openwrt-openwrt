137cb06c87e2cfe110ba9dfdbbad409bb0197955
Chen Minqiang <ptpt52@gmail.com>
2018-04-20 05:47:24 +0800

    new tempe-lite2 support and fixup

--- target/linux/sunxi/image/cortex-a53.mk
+++ target/linux/sunxi/image/cortex-a53.mk
@@ -50,7 +50,7 @@ TARGET_DEVICES += sun50i-h5-orangepi-zero-plus
 
 define Device/sun50i-h6-tempe-h6v2
   DEVICE_TITLE:=Tempe H6 V2
-  SUPPORTED_DEVICES:=tempe,h6-tempe-h6v2
+  SUPPORTED_DEVICES:=tempe,tempe-h6v2
   SUNXI_DTS:=allwinner/sun50i-h6-tempe-h6v2
   KERNEL_NAME := Image
   KERNEL := kernel-bin
@@ -59,17 +59,15 @@ endef
 
 TARGET_DEVICES += sun50i-h6-tempe-h6v2
 
-define Device/sun50i-h6-orangepi-lite2
-  DEVICE_TITLE:=Xunlong Orange Pi Lite2
-  SUPPORTED_DEVICES:=xunlong,h6-orangepi-lite2
-  SUNXI_DTS:=allwinner/sun50i-h6-orangepi-lite2
+define Device/sun50i-h6-tempe-lite2
+  DEVICE_TITLE:=TEMPE Lite 2
+  SUPPORTED_DEVICES:=tempe,tempe-lite2
+  SUNXI_DTS:=allwinner/sun50i-h6-tempe-lite2
   KERNEL_NAME := Image
   KERNEL := kernel-bin
-  IMAGES := sdcard.img.gz
-  IMAGE/sdcard.img.gz := h6-sunxi-sdcard | append-metadata | gzip
-  DEVICE_PACKAGES := blockd kmod-fs-ext4 mkf2fs e2fsprogs fdisk partx-utils
+  DEVICE_PACKAGES := blockd kmod-fs-ext4 mkf2fs e2fsprogs fdisk partx-utils kmod-brcmfmac
 endef
 
-TARGET_DEVICES += sun50i-h6-orangepi-lite2
+TARGET_DEVICES += sun50i-h6-tempe-lite2
 
 endif
